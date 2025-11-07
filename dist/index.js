1770393e617caf55893ef8fb5e4633ae6c75bff1
arthurhenrique-Dev <henriquearthur.dev@gmail.com>
2025-11-07 19:03:04 -0300

    treinamento de ts completo

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,52 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const hw = 'hello world';
 console.log(hw);
+let number = 123;
+console.log(number);
+let boolean = true;
+console.log(boolean);
+let nomes = ['Antonio', 'José'];
+console.log(nomes);
+var Patentes;
+(function (Patentes) {
+    Patentes["Genin"] = "genin";
+    Patentes["Chunin"] = "chunin";
+    Patentes["Jounin"] = "jounin";
+})(Patentes || (Patentes = {}));
+console.log(Patentes.Chunin);
+var Posição;
+(function (Posição) {
+    Posição["Centroavante"] = "centroavante";
+    Posição["Ponta"] = "ponta";
+    Posição["Meio"] = "meio";
+    Posição["Volante"] = "volante";
+    Posição["Lateral"] = "lateral";
+    Posição["Zagueiro"] = "zagueiro";
+    Posição["Goleiro"] = "goleiro";
+})(Posição || (Posição = {}));
+let diMaria = {
+    name: 'Di Maria',
+    posição: Posição.Ponta,
+    número: 11,
+    clube: 'Club Atlético Rosario Central'
+};
+console.log(diMaria);
+let Kakashi = {
+    nome: 'Kakashi',
+    patente: Patentes.Jounin,
+    clã: 'Hatake'
+};
+console.log(Kakashi);
+let base = 45;
+let mutavel = base;
+const NinjaEFutebolista = {
+    name: 'Scorbunny',
+    posição: Posição.Ponta,
+    número: 10,
+    clube: 'Pokemon League',
+    nome: 'Greninja',
+    patente: Patentes.Jounin,
+    clã: 'Tipo água'
+};
+console.log(NinjaEFutebolista);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,69 @@
 const hw: string = 'hello world'
 console.log(hw)
+
+let number: number = 123
+console.log(number)
+
+let boolean: boolean = true
+console.log(boolean)
+
+let nomes: Array<string> = ['Antonio', 'José']
+console.log(nomes)
+
+enum Patentes{
+    Genin = 'genin',
+    Chunin = 'chunin',
+    Jounin = 'jounin'
+}
+console.log(Patentes.Chunin)
+
+enum Posição{
+    Centroavante = 'centroavante',
+    Ponta = 'ponta',
+    Meio = 'meio',
+    Volante = 'volante',
+    Lateral = 'lateral',
+    Zagueiro = 'zagueiro',
+    Goleiro = 'goleiro'
+}
+interface Jogador{
+    name: string,
+    posição: Posição,
+    número: number
+}
+interface Jogador{
+    clube: string
+}
+let diMaria: Jogador ={
+    name: 'Di Maria',
+    posição: Posição.Ponta,
+    número: 11,
+    clube: 'Club Atlético Rosario Central'
+}
+console.log(diMaria)
+type Ninja = {
+    nome: string
+    patente: Patentes
+    clã: string
+}
+let Kakashi: Ninja = {
+    nome: 'Kakashi',    
+    patente: Patentes.Jounin,
+    clã: 'Hatake'
+}
+console.log(Kakashi)
+let base: any = 45 
+let mutavel = <string>base
+
+const NinjaEFutebolista: Jogador & Ninja = {
+    
+    name: 'Scorbunny',
+    posição: Posição.Ponta,
+    número: 10,
+    clube: 'Pokemon League',
+    nome: 'Greninja',    
+    patente: Patentes.Jounin,
+    clã: 'Tipo água'
+
+}
+console.log(NinjaEFutebolista)
